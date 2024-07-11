7ef12dee26dd71782f47bfbd9f3912dc257011d8
nodejs_test <trannamanh170@gmail.com>
2024-07-11 22:41:03 +0700

    push

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    var abc = 123;
+    var b = 2;
+    var d = abc + b;
+    return res.send('Hello World!fdfdfdf lồn')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
