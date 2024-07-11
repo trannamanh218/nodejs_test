62c1efe53458ef1692d20a3917d1bd73666fcdc6
nodejs_test <trannamanh170@gmail.com>
2024-07-11 22:44:56 +0700

    push

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     var abc = 123;
     var b = 2;
     var d = abc + b;
-    return res.send('Hello World! ')
+    return res.send('Hello World! ok đấy ')
 })
 
 app.listen(port, () => {
