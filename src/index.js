3c000d654124a2dbffbd2a57b8065db89ef524a5
nodejs_test <trannamanh170@gmail.com>
2024-07-17 21:44:14 +0700

    node cmt, cấu hình route , controller

--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ const router = require('./routes')
 
 const port = 3000
 
+
+// cấu hình đường dẫn có thể path được ảnh /img/abc.jpg
 app.use(express.static(path.join(__dirname, 'public')))
 
-//sử dụng milldaware
+//sử dụng milldaware 
+//Middleware express.urlencoded sẽ phân tích dữ liệu này và thêm nó vào đối tượng req.body, 
+//cho phép bạn truy cập các giá trị trong form bằng cách truy cập vào các thuộc tính của req.body
 app.use(express.urlencoded(
     {
         extended: true,
@@ -19,15 +23,15 @@ app.use(express.urlencoded(
 app.use(express.json())
 
 
-//http loger
-
+//http loger -> log trong teminal khi load lại trang
 // app.use(morgan('combined'))
 
-//cấu hình tempalte engine
+//cấu hình tempalte engine -> cấu hình đuôi file handlebar cho ngắn 
 app.engine('hbs', handlebars({
     extname: '.hbs',
 }));
 
+// cấu hình hiển thị file index
 app.set('view engine', 'hbs')
 
 app.set('views', path.join(__dirname, 'resources/views'))
